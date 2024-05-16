f58d1bab4b04453220ef84f38ef3868e55347bc4
Preetam Saha <82079602+preetamjumech@users.noreply.github.com>
2024-05-16 10:28:52 +0000

    fixing train-test-split

--- train_model.py
+++ train_model.py
@@ -33,8 +33,6 @@ X = data.drop(["target"],axis = 1).values.astype("float32")
 y = data["target"].values.astype("int32")
 X_train,X_test, y_train, y_test = train_test_split(X,y, test_size=0.2,random_state=42)
 
-X_train,X_test, y_train, y_test = train_test_split(data, test_size=0.2,random_state=42)
-
 #Logistic Regression
 logreg = LogisticRegression(C= 0.0001, solver = "lbfgs", max_iter= 100,multi_class= "multinomial")
 logreg.fit(X_train,y_train)
