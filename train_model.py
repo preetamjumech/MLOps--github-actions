003fcee7751b38cc312f49716072b71f3c999a4f
Preetam Saha <82079602+preetamjumech@users.noreply.github.com>
2024-05-16 03:34:08 +0000

    making scaffold

---
+++ train_model.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, f1_score, recall_score, precision_score
+
+#Load data
